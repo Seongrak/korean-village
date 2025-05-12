3558239dad670c4a8292b877bb058c015ecbb7d4
Seongrak Choi <rocky@mac.home>
2025-05-12 00:59:27 -0400

    animation for introduction

--- src/pages/HomeSections/IntroductionSection.jsx
+++ src/pages/HomeSections/IntroductionSection.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 function IntroductionSection() {
+  const introRef = useRef();
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsVisible(entry.isIntersecting);
+      },
+      { threshold: 0.4 }
+    );
+
+    const current = introRef.current;
+    if (current) observer.observe(current);
+
+    return () => {
+      if (current) observer.unobserve(current);
+    };
+  }, []);
+
   return (
     <section className="intro-section">
       <img src="../boss.jpeg" alt="Owner and Family" className="intro-bg" />
       <div className="intro-overlay">
-        <div className="intro-content">
+        <div
+          className={`intro-content ${isVisible ? "visible" : ""}`}
+          ref={introRef}
+        >
           <h2>More Than a Restaurant, It’s a Legacy</h2>
           <p>
             Korean Village has been proudly family-owned and operated since{" "}
